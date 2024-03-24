e64b888e15f42f2ad4540716d82bd7c7038eed22
“Harsh <harshwalia449@gmail.com>
2024-03-24 18:15:40 +0530

    placed ad badge over ad

--- src/app/advertisement.tsx
+++ src/app/advertisement.tsx
@@ -2,8 +2,13 @@ import Image from 'next/image'
 const Advertisement: React.FC = () => {
     return (
         <div className="  w-1 p-2 m-1 shadow-md  bg-[#fff] dark:bg-cardDark">
-            <Image src="/dicken.png" width={500} height={300} alt='ad' />
-            <h1 className='pl-[9px] pr-[4px] text-[20px] text-center dark:text-textDark'>Advertisement title </h1>
+            <div className="relative ">
+                <div className="absolute bg-black text-white px-4 py-1 top-0 right-0 ">Ad</div>
+                <Image src="/dicken.png" width={500} height={300} alt='ad' />
+
+            </div>
+
+            <h1 className='pl-[9px] pr-[4px] font-[600] text-[20px] mt-2 mb-2 dark:text-textDark'>Advertisement title </h1>
             <p className="text-sm  pl-[9px] dark:text-textDark">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ipsum dolorum temporibus, est minus exercitationem fugit beatae impedit quae eaque unde saepe corporis. Dicta nesciunt sequi culpa ex deserunt fuga officiis! Placeat, saepe? Enim, rem quasi
 
             </p>
